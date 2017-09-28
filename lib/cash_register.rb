1746fb2461e32788f0320ff5b79554715b5f30cd
Hannah Danziger <hdanziger8@gmail.com>
2017-09-28 18:33:05 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,8 +10,10 @@ end
 
 def add_item (title, price, quantity=1)
   self.total = price * quantity
-  self.times do |item|
+  quantity.times do
+    item << title
   end
+  self.last_transaction = title * quantity
 end
 
 
